3e580c24bbb03e420b7e74ee82d6098f174a395a
maikmedeirosm <maikmedeirosm@outlook.com>
2018-06-20 17:01:31 -0400

    excel

--- LOJA/visualizar-produto.php
+++ LOJA/visualizar-produto.php
@@ -4,8 +4,15 @@
 		include("banco-produto.php");
     include("mostra-alerta.php");
 	 	$produtos = listaProduto($conexao);
+    
+    if($_REQUEST["excel" == "true"]{
+      header("Content-type: application/vnd.ms-excel");
+      header("Content-type: application/force-download");
+      header("Content-Disposition: attachment; filename=atadobimestre.xls");
+      header("Pragma: no-cache");
+    }
 	?>
-
+  <!-- http://www.tiagomatos.com/blog/exportando-dados-tabulares-html-em-arquivo-do-excel -->
 	<table class="table table-striped table-bordered">
 		<tr>
 				<td>Id</td>
@@ -31,4 +38,11 @@
 	    </tr>
 	<?php endforeach?>
 	</table>
+  <?php
+    if($_REQUEST <> "true"){
+  ?>
+  <form action="/?excel=true">
+    <input type="submit" value="Gerar em Excel"  
+  </form>
+<?php   }  ?>
 	<?php include("rodape.php");?>
